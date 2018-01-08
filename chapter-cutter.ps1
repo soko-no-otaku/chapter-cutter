485a3aca33dbfa5a20935e1daf0bca017571d65c
soko-no-otaku <soko.no.otaku@gmail.com>
2018-01-08 14:34:42 +0900

    Add files via upload

---
+++ chapter-cutter.ps1
@@ -0,0 +1,11 @@
+﻿$source  = $args[0]
+$chapter = $args[1]
+$output_filename = (Read-Host "Please enter output file name") + ".mp4"
+
+$chapter_list = (.\ffprobe.exe -print_format json -show_chapters $source | ConvertFrom-Json)[0].chapters
+
+$start_time = $chapter_list[$chapter - 1].start_time
+$end_time   = $chapter_list[$chapter - 1].end_time
+$duration   = $end_time - $start_time
+
+.\ffmpeg.exe -ss $start_time -i $source -t $duration $output_filename
