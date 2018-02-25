a1844c8fef28003862161c189ecae1ed17b62268
soko-no-otaku <evoltaro@gmail.com>
2018-02-25 23:11:08 +0900

    連続チャプター指定に対応

--- chapter-cutter.ps1
+++ chapter-cutter.ps1
@@ -1,11 +1,17 @@
 ﻿$source  = $args[0]
-$chapter = $args[1]
+$start_chapter = $args[1]
+if ($args.Length -eq 2) {
+    $end_chapter = $start_chapter
+} else {
+    $end_chapter = $args[2]
+}
+
 $output_filename = (Read-Host "Please enter output file name") + ".mp4"
 
 $chapter_list = (.\ffprobe.exe -print_format json -show_chapters $source | ConvertFrom-Json)[0].chapters
 
-$start_time = $chapter_list[$chapter - 1].start_time
-$end_time   = $chapter_list[$chapter - 1].end_time
+$start_time = $chapter_list[$start_chapter - 1].start_time
+$end_time   = $chapter_list[$end_chapter - 1].end_time
 $duration   = $end_time - $start_time
 
 .\ffmpeg.exe -ss $start_time -i $source -t $duration $output_filename
